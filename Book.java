23bc8059d076c038e9c9515ef026cbda6393cc4d
Đỗ Phúc Danh <pepsipro159357@gmail.com>
2026-05-26 19:58:38 +0700

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    public void printInfo(){
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
